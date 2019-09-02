8e6de3af1c5a60c79160282bbca63d552e305d6f
John F. DeLancey <john@whiteraventechnology.com>
2019-09-02 19:12:26 +0000

    Moved polling and parsing to dragon-tools.js, added 7 Day chart

--- server.js
+++ server.js
@@ -1,43 +1,10 @@
+const tools = require('./dragon-tools')
+const moment = require('moment')
 const util = require('util');
 const dcsdk = require('dragonchain-sdk')
-const moment = require('moment');
 const _ = require('underscore');
 const express = require('express');
 const app = express();
-
-const parseBlocksByDay = function (blocks) {
-	const occurences = [];
-
-	blocks.forEach(function (element) {
-		occurences.push(element.header.timestamp * 1000);
-	});
-
-	return _.map(
-		_.groupBy(occurences, function (timestamp) {
-			return moment(timestamp).startOf('day').format();
-		}),
-		function (group, day) {
-			return {day: day, times: group}
-		}
-	);
-}
-
-const parseBlocksByHour = function (blocks) {
-        const occurences = [];
-
-        blocks.forEach(function (element) {
-                occurences.push(element.header.timestamp * 1000);
-        });
-
-        return _.map(
-                _.groupBy(occurences, function (timestamp) {
-                        return moment(timestamp).startOf('hour').format("HH:mm");
-                }),
-                function (group, hour) {
-                        return {hour: hour, times: group}
-                }
-        );
-}
 
 
 const main = async() => {
@@ -53,44 +20,12 @@ const main = async() => {
 		}
 	}
 
-	const poll = async() => {
-		const status_response = await client.getStatus();
-		const last_block_response = await client.queryBlocks({ limit: 1, sort: "block_id:desc"});
-
-		let blocks_day = [];
-		let done = false;
-		let page = 0;
-		let dayAgoTimestamp = moment().subtract(1,'d').startOf('hour').format("X");
-
-		while (!done) {
-			const blocks_response = await client.queryBlocks({ limit: 50, offset: page, sort: "block_id:desc"});
-
-			for (let i = 0; i < blocks_response.response.results.length; i++)
-			{
-				element = blocks_response.response.results[i];
-
-				if (element.header.timestamp >= dayAgoTimestamp)
-					blocks_day.push(element);
-				else
-				{
-					done = true;
-					break;
-				}
-			}
-
-			page += 50;
-		}
-
-		return {status: status_response.response, last_block: last_block_response.response.results[0], blocks_day};
-	}
-
-
 	app.set('view engine', 'pug');
 
 	app.get('/', awaitHandlerFactory(async (req, res) => {
-		const poll_response = await poll();
+		const poll_response = await tools.poll(client);
 
-		const blocksByHour = parseBlocksByHour(poll_response.blocks_day);
+		const blocksByHour = tools.parseBlocksByHour(poll_response.blocks_day);
 
 		res.render('index', {
 			title: "Dragonchain UVN Block Explorer",
@@ -108,13 +43,14 @@ const main = async() => {
 			block_height: poll_response.last_block.header.block_id,
 			blocks: poll_response.blocks_day,
 			blocks_by_hour: blocksByHour,
+			blocks_by_day: poll_response.blocks_week,
 			blocks_last_24_hours: poll_response.blocks_day.length
 		});
 	}));
 
 
-	// If SSL available, use port 443 //
-	const server = app.listen(80, () => {
+	// In production use port 80 or, if SSL available, use port 443 //
+	const server = app.listen(3000, () => {
 		console.log(`Express running → PORT ${server.address().port}`);
 	});
 }
